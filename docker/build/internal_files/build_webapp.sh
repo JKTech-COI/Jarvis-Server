32e1a9c56e5f3d9d70eca5bd1df63ca407fab36e
Guruprasad.J <Guruprasad.J@jktech.com>
2023-01-27 12:38:59 +0530

    df sh

--- docker/build/internal_files/build_webapp.sh
+++ docker/build/internal_files/build_webapp.sh
@@ -5,6 +5,8 @@ set -e
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
+unset NPM_CONFIG_PREFIX
+
 nvm install 14.17.0
 nvm use 14.17.0
 
